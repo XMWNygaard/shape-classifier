25509c6af1db4070ee44a1f17adee8027bb0609b
Mikkel Wexøe Ertbjerg <mertbjerg@xolta.com>
2024-04-22 21:58:13 +0200

    updated existing unittest to use double

--- ShapeIdentifier.Tests/TriangleTest.cs
+++ ShapeIdentifier.Tests/TriangleTest.cs
@@ -5,27 +5,20 @@ namespace ShapeIdentifier.Tests
     [TestClass]
     public class TriangleTest
     {
-        private Triangle _cut;
-
-        [TestInitialize]
-        public void Initialize()
-        {
-            _cut = new Triangle();
-        }
 
         [TestMethod]
-        [DataRow(1, 1, 1)]
-        [DataRow(1, 2, 2)]
-        [DataRow(5, 4, 2)]
+        [DataRow(1.0, 1.0, 1.0)]
+        [DataRow(1.0, 2.0, 2.0)]
+        [DataRow(5.0, 4.0, 2.0)]
         [DataRow(1.5, 1.5, 1.5)]
         [DataRow(1.3, 1.3, 1.1)]
         [DataRow(1.9, 1.2, 1.8)]
         [DataRow(20.123, 20.123, 20.123)]
         [DataRow(50.223, 75.98885757, 50.223)]
         [DataRow(102.558899, 150.918273456, 124.99532)]
-        public void IsValidBySides_ShouldReturnTrue_WhenTwoSidesSummedAreGreaterThanTheThirdSide(decimal a, decimal b, decimal c)
+        public void IsValidBySides_ShouldReturnTrue_WhenTwoSidesSummedAreGreaterThanTheThirdSide(double a, double b, double c)
         {
-            Assert.IsTrue(_cut.IsValidBySides(a, b, c));
+            Assert.IsTrue(Triangle.IsValidBySides(a, b, c));
         }
 
         [TestMethod]

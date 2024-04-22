e6c8ddb0fa22ceccc2061b5f6ac5e36294a9a486
Mikkel Wexøe Ertbjerg <mertbjerg@xolta.com>
2024-04-22 21:18:56 +0200

    added test project. Removed shape exception

---
+++ ShapeIdentifier.Tests/TriangleTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShapeIdentifier.Tests
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
